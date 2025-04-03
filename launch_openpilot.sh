2d491e139f7ca711652319db84f4e6b33934c8c0
Windirt Young <windirt@gmail.com>
2025-04-03 12:50:00 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
+export ATHENA_HOST='wss://athena.konik.ai'
+export API_HOST='https://api.konik.ai'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+
 exec ./launch_chffrplus.sh
