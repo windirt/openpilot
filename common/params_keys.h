zuowenjing/dragonpilot

Hide HUD - init

common/params_keys.h
@@ -117,4 +117,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"UpdaterLastFetchTime", PERSISTENT},
     {"Version", PERSISTENT},
+    {"dp_general_hide_hud_speed_kph", PERSISTENT},
 };

selfdrive/ui/qt/offroad/dp_panel.cc
@@ -9,10 +9,12 @@ void DPPanel::add_general_toggles() {
       "",
     },
   };
+  auto hide_hud = new ParamSpinBoxControl("dp_general_hide_hud_speed_kph", tr("Hide HUD When Moves above:"), tr("To prevent screen burn-in, hide Speed, MAX Speed, and Steering/DM Icons when the car moves.\nOff = Stock Behavior\n1 km/h ≈ 0.6 mph"), "", 0, 120, 5, tr(" km/h"), tr("Off"));
   for (auto &[param, title, desc] : toggle_defs) {
     if (param == "") {
       auto label = new LabelControl(title, "");
       addItem(label);
+      addItem(hide_hud);
       continue;
     }
     auto toggle = new ParamControl(param, title, desc, "", this);

selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -130,9 +130,15 @@ void AnnotatedCameraWidget::paintGL() {
   painter.setPen(Qt::NoPen);
 
   model.draw(painter, rect());
-  dmon.draw(painter, rect());
-  hud.updateState(*s);
-  hud.draw(painter, rect());
+  bool hide_hud = s->scene.dp_general_hide_hud_speed_kph > 0 && sm["carState"].getCarState().getVEgo() > s->scene.dp_general_hide_hud_speed_kph * 0.278;
+  if (!hide_hud) {
+    dmon.draw(painter, rect());
+    hud.updateState(*s);
+    hud.draw(painter, rect());
+    experimental_btn->setVisible(true);
+  } else {
+    experimental_btn->setVisible(false);
+  }
 
   double cur_draw_t = millis_since_boot();
   double dt = cur_draw_t - prev_draw_t;

selfdrive/ui/ui.cc
@@ -65,6 +65,7 @@ static void update_state(UIState *s) {
 void ui_update_params(UIState *s) {
   auto params = Params();
   s->scene.is_metric = params.getBool("IsMetric");
+  s->scene.dp_general_hide_hud_speed_kph = std::atoi(params.get("dp_general_hide_hud_speed_kph").c_str());
 }
 
 void UIState::updateStatus() {

windirt/openpilot

common/params_keys.h
@@ -119,4 +119,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"dp_device_last_log", CLEAR_ON_MANAGER_START},
     {"dp_device_reset_conf", CLEAR_ON_MANAGER_START},
+    {"dp_ui_hide_hud_speed_kph", PERSISTENT},
 };

selfdrive/ui/qt/offroad/dp_panel.cc
@@ -9,10 +9,12 @@ void DPPanel::add_general_toggles() {
       "",
     },
   };
+  auto hide_hud = new ParamSpinBoxControl("dp_ui_hide_hud_speed_kph", tr("Hide HUD When Moves above:"), tr("To prevent screen burn-in, hide Speed, MAX Speed, and Steering/DM Icons when the car moves.\nOff = Stock Behavior\n1 km/h ≈ 0.6 mph"), "", 0, 120, 5, tr(" km/h"), tr("Off"));
   for (auto &[param, title, desc] : toggle_defs) {
     if (param == "") {
       auto label = new LabelControl(title, "");
       addItem(label);
+      addItem(hide_hud);
       continue;
     }
     auto toggle = new ParamControl(param, title, desc, "", this);

selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -130,9 +130,15 @@ void AnnotatedCameraWidget::paintGL() {
   painter.setPen(Qt::NoPen);
 
   model.draw(painter, rect());
-  dmon.draw(painter, rect());
-  hud.updateState(*s);
-  hud.draw(painter, rect());
+  bool hide_hud = s->scene.dp_ui_hide_hud_speed_kph > 0 && sm["carState"].getCarState().getVEgo() > s->scene.dp_ui_hide_hud_speed_kph * 0.278;
+  if (!hide_hud) {
+    dmon.draw(painter, rect());
+    hud.updateState(*s);
+    hud.draw(painter, rect());
+    experimental_btn->setVisible(true);
+  } else {
+    experimental_btn->setVisible(false);
+  }
 
   double cur_draw_t = millis_since_boot();
   double dt = cur_draw_t - prev_draw_t;

selfdrive/ui/ui.cc
@@ -65,6 +65,7 @@ static void update_state(UIState *s) {
 void ui_update_params(UIState *s) {
   auto params = Params();
   s->scene.is_metric = params.getBool("IsMetric");
+  s->scene.dp_ui_hide_hud_speed_kph = std::atoi(params.get("dp_ui_hide_hud_speed_kph").c_str());
 }
 
 void UIState::updateStatus() {