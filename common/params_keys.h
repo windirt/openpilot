zuowenjing/dragonpilot

Hide HUD - init

common/params_keys.h
@@ -117,4 +117,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"UpdaterLastFetchTime", PERSISTENT},
     {"Version", PERSISTENT},
+    {"dp_general_hide_hud_speed_kph", PERSISTENT},
 };

windirt/openpilot

common/params_keys.h
@@ -122,4 +122,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"dp_device_last_log", CLEAR_ON_ONROAD_TRANSITION},
     {"dp_device_reset_conf", CLEAR_ON_MANAGER_START},
+    {"dp_ui_hide_hud_speed_kph", PERSISTENT},
 };